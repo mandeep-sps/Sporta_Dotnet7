34d5ae256fde911b0ebf37a03225c3f3fe96d7f9
mandeep-sps <mandeep_singh1@softprodigy.com>
2022-11-17 17:56:16 +0530

    Added compressions.

--- Sporta.WebApp/Program.cs
+++ Sporta.WebApp/Program.cs
@@ -1,14 +1,32 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
 using Sporta.WebApp;
+using System.IO.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
 
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.SmallestSize;
+});
 var startup = new Startup(builder.Configuration);
 
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
+app.UseResponseCompression();
 startup.Configure(app, app.Environment);
 app.MapRazorPages();
 await app.RunAsync();
